aa62e1ce0a5e28e516e42c144e306dd24721a615
Ster1402 <56319959+Ster1402@users.noreply.github.com>
2022-04-23 03:39:33 +0100

    Update diagram and first setup

---
+++ main.py
@@ -0,0 +1,13 @@
+from utils import ReconizerProcess
+
+import sys
+
+
+def main(args):
+	pass
+
+
+
+
+if __name__ == '__main__':
+	main(sys.argv)
---
+++ utils/ReconizerProcess.py
@@ -0,0 +1,8 @@
+
+class ReconizerProcess:
+
+	def __init__(self):
+		pass
+
+	def recognitionThread(self):
+		pass
