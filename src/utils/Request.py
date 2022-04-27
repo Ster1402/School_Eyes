f1805db6aef246ef72fe9b738d0f6b785ce07d08
Ster1402 <56319959+Ster1402@users.noreply.github.com>
2022-04-27 18:32:03 +0100

    Database queries management

---
+++ src/utils/Request.py
@@ -0,0 +1,46 @@
+""" Request : <dict>
+        {
+                id : <id_enseignant>,
+                teacher : <nom_enseignant>,
+                classroom : <salle_de_cours>,
+                course : <cours_en_question>,
+                level : <niveau_etudiants>,
+                disciplines : [
+                        {
+                                name : <nom_filiere>,
+                                axes : [<nom_des_axes>, ...]
+                        },
+                        ...
+                ],
+                socket_id : <id_du_socketio>,
+                finish_hour : <heure_de_fin>
+        }
+"""
+
+class Request(dict):
+
+    def __init__(self, id, teacher, classroom, course, 
+                disciplines, level, socket_id, finish_hour):
+
+        self._verifyParam(id, teacher, classroom, course, 
+                         disciplines, level, socket_id, finish_hour)
+
+        self["id"] = id
+        self["teacher"] = teacher
+        self["classroom"] = classroom
+        self["course"] = course
+        self["disciplines"] = disciplines
+        self["level"] = level
+        self["socket_id"] = socket_id
+        self["finish_hour"] = finish_hour
+
+    def _verifyParam(self, id, teacher, classroom, course, 
+                disciplines, level, socket_id, finish_hour):
+                assert isinstance(id, int)
+                assert isinstance(socket_id, str)
+                assert isinstance(teacher, str)
+                assert isinstance(classroom, str)
+                assert isinstance(course, str)
+                assert isinstance(disciplines, list) or isinstance(disciplines, set)
+                assert isinstance(level, int)
+
